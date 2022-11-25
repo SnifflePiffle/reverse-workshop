f039852a44b9e7707c9fef9cffd4317c193e57fa
SnifflePiffle <94315580+SnifflePiffle@users.noreply.github.com>
2022-11-25 09:41:23 +0200

    Add files via upload

---
+++ reverse.sh
@@ -0,0 +1,13 @@
+#Go to home directory
+cd ~
+
+#Install pwndbg
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+sudo ./setup.sh
+
+#Install ltrace
+sudo apt install ltrace
+
+#Install ghidra
+sudo apt install ghidra
